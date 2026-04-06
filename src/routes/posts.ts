b61c05fc7e6d6b81540734a73c7ef51dc08960e6
David Lindblom <lindblom.david@hotmail.com>
2026-04-06 13:35:59 +0200

    branch created

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -4,6 +4,6 @@ import { getPosts } from "../controllers/postsController.js"
 const postsRouter = express.Router()
 
 postsRouter.get("/", getPosts)
-//Här kommer post, patch och delete också
+//Här kommer post, patch och delete också!
 
 export default postsRouter
