509deb842286e8d171d0a18df1defa58d77322fc
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-09 10:26:15 +0200

    la till cors och morgan (#38)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express"
+import cors from "cors"
+import morgan from "morgan"
 import postsRouter from "./routes/posts.js"
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
+app.use(morgan("dev"))
 
 const apiRouter = express.Router()
 app.use("/api/v1", apiRouter)
