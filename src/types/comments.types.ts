12576bf8ea4670ce4e989459f2f01cb5171227c6
David Lindblom <lindblom.david@hotmail.com>
2026-04-10 08:15:41 +0100

    added comments to types and made mock-comments

--- src/types/comments.types.ts
+++ src/types/comments.types.ts
@@ -1,16 +1,22 @@
+
+// everything a comment needs
 export interface Comment {
   id: number,
-  postId: number,
+  postId: number, // matches the post it belongs to
   userId: string,
   isAnonymous: boolean,
-  isPsychologist: boolean,
+  isPsychologist: boolean, // UI highlights/marks comment
   content: string,
   createdAt: string,
-  isEdited: boolean,
+  isEdited: boolean, // UI shows "(edited)"
   likes: number,
   dislikes: number,
 }
 
+
+// parameters from the URL (to identify which post and comment)
 export type CommentParams = { postId: string; commentId: string }
 
+
+// what the frontend sends when creating a comment (backend sets the rest)
 export type CreateCommentBody = Omit<Comment, "id" | "createdAt" | "likes" | "dislikes" | "isEdited">
