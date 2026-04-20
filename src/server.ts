8ad60038b47872d689ff3f28cce8c60b413b32fa
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-20 13:36:13 +0200

    startat mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import app from "./app.js";
-const PORT = process.env.HTTP_PORT
+const PORT = process.env.HTTP_PORT || 3000
+const MONGODB_URI = process.env.MONGODB_URI
+
+if(!MONGODB_URI) {
+  console.error("MONGODB_URI is missing in .env")
+  process.exit(1)
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}`)
