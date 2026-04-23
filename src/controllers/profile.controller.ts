f89b3f9cf507c1ecd18c81f5257cbe01a79cfa42
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-23 08:34:39 +0200

    skapat getProfile logik

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,7 +1,17 @@
 import { Controller } from "../types/index.types.js"
+import { createError } from "../utils/createError.js"
 
 export const getProfile: Controller = async (req, res) => {
+  const user = req.user
 
+  if(!user) {
+    throw createError("User not found", 404, "USER_NOT_FOUND")
+  }
+
+  res.json({
+    success: true,
+    data: user
+  })
 }
 
 export const updateProfile: Controller = async (req, res) => {
