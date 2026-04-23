89dcd28bfc9dddae71f73d00c619a4e0064a3a00
David Lindblom <lindblom.david@hotmail.com>
2026-04-23 11:49:56 +0200

    add /google route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { checkAdmin } from "../middleware/checkAdmin.js";
 import { checkAuth } from "../middleware/checkAuth.js";
-import { createUser, loginUser, getUserByUsername, getAllUsers, deleteUserById } from "../controllers/users.controller.js";
+import { createUser, loginUser, getUserByUsername, getAllUsers, deleteUserById, googleLogin } from "../controllers/users.controller.js";
 
 const usersRouter = express.Router()
 
 //Public routes
+usersRouter.post("/google", googleLogin)
 usersRouter.post("/register", createUser)
 usersRouter.post("/login", loginUser)
 usersRouter.get("/:username", getUserByUsername)
