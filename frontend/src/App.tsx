f88b202c722773faba54be9a80dfc04e9625b6ec
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-28 11:09:58 +0200

    skapat header för mobil

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
-import Login from './pages/Login'
+import Layout from './layout/Layout'
 
 function App() {
-
-  return <Login />
+  return(
+    <Layout/>
+  )
 }
 
 export default App
---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import type { ButtonHTMLAttributes } from "react"
+import s from "./Button.module.css"
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode,
+  onClick?: () => void,
+  variant?: "primary" | "secondary" | "transparent"
+  size?: "normal" | "small" | "x-small"
+}
+
+export default function Button({variant = "primary", size = "normal", onClick, children}: ButtonProps) {
+  return(
+    <button
+      className={`${s.base} ${s[size]} ${s[variant]}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
