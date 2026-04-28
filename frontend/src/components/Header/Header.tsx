a9fed1a56f0a9b37fca470afe886aa5d46687818
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-28 17:33:18 +0200

    implement navigation

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,36 +1,93 @@
+import { useEffect } from "react";
 import { useTheme } from "../../context/useTheme";
-import { IoMenu } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 import Button from "../Button/Button"
 import logo from "../../assets/logo.png"
 import logoDark from "../../assets/logo-dark.png"
 import s from "./Header.module.css"
+import { useState } from "react";
+import { mainMenu, secondaryMenu } from "./menuItems";
+import { NavLink } from "react-router";
 
 export default function Header() {
   const { resolvedTheme } = useTheme()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+  const closeMenu = () => setIsMenuOpen(false)
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset"
+    }
+  }, [isMenuOpen])
 
   return(
-    <header className={s.header}>
-      <div className={s.logoWrapper}>
-        <img 
-          src={resolvedTheme === "dark" ? logoDark : logo} 
-          className={s.logo} 
-        />
-        <p className={s.logoText}>Prata Ut</p>
-      </div>
+    <>
+      <header className={s.header}>
+        <div className={s.logoWrapper}>
+          <img 
+            src={resolvedTheme === "dark" ? logoDark : logo} 
+            className={s.logo} 
+          />
+          <p className={s.logoText}>Prata Ut</p>
+        </div>
 
-      <div className={s.buttonWrapper}>
-        <Button
-          size="small"
-        >
-          Skapa konto
-        </Button>
-        <Button
-          variant="transparent"
-          size="x-small"
-        >
-          <IoMenu size={24}/>
-        </Button>
-      </div>
-    </header>
+        <div className={s.buttonWrapper}>
+          <Button
+            size="small"
+          >
+            Skapa konto
+          </Button>
+          <Button
+            variant="transparent"
+            size="x-small"
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? <IoClose size={24} /> : <IoMenu size={24}/>}
+          </Button>
+        </div>
+      </header>
+
+      {isMenuOpen &&
+        <nav className={s.navContainer}>
+          <ul className={s.navList}>
+            <div className={s.navWrapper}>
+              {mainMenu.map((link) => (
+                <li key={link.path}>
+                  <NavLink
+                    to={link.path}
+                    onClick={closeMenu}
+                    className={({isActive}) => (isActive ? s.active : "")}
+                  >
+                    {link.title}
+                  </NavLink>
+                </li>
+              ))}
+            </div>
+
+            <div className={s.navWrapper}>
+              {secondaryMenu.map((link) => (
+                <li key={link.path}>
+                  <NavLink
+                    to={link.path}
+                    onClick={closeMenu}
+                    className={({isActive}) => (isActive ? s.isActive : "")}
+                  >
+                    {link.title}
+                  </NavLink>
+                </li>
+              ))}
+            </div>
+          </ul>
+        </nav>
+      }
+    </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {GoogleOAuthProvider} from "@react-oauth/google"
+import { BrowserRouter } from 'react-router'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import App from './App.tsx'
 
@@ -9,9 +10,11 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId='190516201423-jk46485nrchrf1j0dq6tv16ht4bnkjq6.apps.googleusercontent.com'>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
