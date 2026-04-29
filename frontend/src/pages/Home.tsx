3dabb52562cad03d5d4219e2c6f055acdb33e21c
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-04-29 11:59:32 +0200

    add short about on home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import Card from "../components/Card/Card";
+
 export default function Home() {
   return(
-    <p>Startsida</p>
+    <>
+      <Card variant="neutral">
+        <h2>Du är inte ensam om dina känslor</h2>
+        <p>PrataUt är en trygg plats där du kan dela dina utmaningar, få stöd från andra och bygga mod att söka professionell hjälp.</p>
+      </Card>
+    </>
   )
 }
