397983e67d2dade6996771956fe703704a2be3ca
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-05-11 13:22:44 +0200

    change to variable instead of listing category and trigger arrays

--- shared/schema/post/atoms.ts
+++ shared/schema/post/atoms.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { TRIGGER_OPTIONS, CATEGORY_OPTIONS } from "../../constants.js"
 
 // --- Helpers ---
 const toLowerCaseArray = z.array(z.string().toLowerCase().trim())
@@ -19,15 +20,13 @@ export const title = z.string().trim().min(1, "Title is required").max(100, "Tit
 
 export const description = z.string().trim().min(1, "Description is required").max(1000, "Description cannot be longer than 1000 characters")
 
-const CATEGORIES = ["relationships", "family", "parenting", "stress", "anxiety", "loneliness", "grief-and-loss", "depression", "other"] as const
-
 export const categories = toLowerCaseArray.pipe(
-  z.array(z.enum(CATEGORIES)).min(1, "At least one category is required")
+  z.array(z.enum(CATEGORY_OPTIONS)).min(1, "At least one category is required")
 )
 
 export const triggerTags = toLowerCaseArray.pipe(
   z.array(
-    z.enum(["self-harm", "suicidal-thoughts", "substance-abuse", "gambling", "eating-disorders", "body-image", "abuse", "domestic-violence", "trauma"])
+    z.enum(TRIGGER_OPTIONS)
   )
 ).default([])
 
@@ -35,5 +34,5 @@ export const likedBy = z.array(z.string()).default([])
 
 // --- Query Atoms ---
 // Ingen min(1) här som på "categories". main page behöver kunna hämta inlägg utan att bestämma kategori
-export const categoriesQuery = stringOrArray(toLowerCaseArray.pipe(z.array(z.enum(CATEGORIES))))
+export const categoriesQuery = stringOrArray(toLowerCaseArray.pipe(z.array(z.enum(CATEGORY_OPTIONS))))
 export const triggerTagsQuery = stringOrArray(triggerTags)
