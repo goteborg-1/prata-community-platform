64c8bdbd208f635f46d3d0cc7444710e6660de59
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-05-18 12:13:19 +0200

    fix deletepost updates tanstack cache

--- frontend/src/hooks/useDeletePost.ts
+++ frontend/src/hooks/useDeletePost.ts
@@ -1,17 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { useNavigate } from "react-router"
 import { deletePost } from "../api/posts.api"
 
 export function useDeletePost() {
   const queryClient = useQueryClient()
-  const navigate = useNavigate()
 
-  return useMutation({
+  return useMutation({    
     mutationFn: deletePost,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["posts"] })
       queryClient.invalidateQueries({ queryKey: ["post"]})
-      navigate("/")
     },
   })
 }
